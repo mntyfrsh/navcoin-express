4a3d3f089079152ad926b5b4dfbd7f121a736905
Ryan Manikowski <ryan@prosoundsystem.com>
2019-05-10 22:16:45 -0400

    fix script

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-log=/home/odroid/navdroid/express/log/reboot.log
-touch $log
-chmod 777 $log
-
-now=$(date +"%m-%d-%Y %T")
-
-echo "${now} - rebooting" >> $log
-
 /usr/sbin/sudo /sbin/shutdown -r now >> $log
 
-echo "script finished" >> $log
-
+exit 0
